eaa62469a47a847d81dcdb24e9af67777efb7b30
Walter Nascimento <walter.nascimento.barroso@gmail.com>
2023-03-14 22:54:35 +0000

    🎨 IMPROVE: Adjust custom class

--- src/components/AsideMenu/index.tsx
+++ src/components/AsideMenu/index.tsx
@@ -5,9 +5,9 @@ const AsideMenu = () => {
       className="fixed hidden z-20 h-full top-0 left-0 pt-16 flex lg:flex flex-shrink-0 flex-col w-64 transition-width duration-75"
       aria-label="Sidebar"
     >
-      <div className="bg-white text-gray-700 dark:bg-gray-800 relative flex-1 flex flex-col min-h-0 border-r border-gray-200 dark:border-gray-700 pt-0">
+      <div className="custom--bg relative flex-1 flex flex-col min-h-0 border-r pt-0">
         <div className="flex-1 flex flex-col pt-5 pb-4 overflow-y-auto">
-          <div className="bg-white text-gray-700 dark:bg-gray-800 flex-1 px-3 divide-y space-y-1">
+          <div className="custom--bg flex-1 px-3 divide-y space-y-1">
             <ul className="space-y-2 pb-2">
               <li>
                 <form action="#" method="GET" className="lg:hidden">
@@ -38,10 +38,10 @@ const AsideMenu = () => {
               <li>
                 <a
                   href="#"
-                  className="text-base text-gray-900 dark:text-gray-200 hover:text-gray-800 dark:hover:text-gray-900 font-normal rounded-lg flex items-center p-2 hover:bg-gray-100 group"
+                  className="custom--link rounded-lg flex items-center p-2"
                 >
                   <svg
-                    className="w-6 h-6 text-gray-500 group-hover:text-gray-900 transition duration-75"
+                    className="w-6 h-6 custom--svg"
                     fill="currentColor"
                     viewBox="0 0 20 20"
                     xmlns="http://www.w3.org/2000/svg"
@@ -49,26 +49,8 @@ const AsideMenu = () => {
                     <path d="M2 10a8 8 0 018-8v8h8a8 8 0 11-16 0z"></path>
                     <path d="M12 2.252A8.014 8.014 0 0117.748 8H12V2.252z"></path>
                   </svg>
-                  <span className="ml-3">Dashboard</span>
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#"
-                  target="_blank"
-                  className="text-base text-gray-900 dark:text-gray-200 hover:text-gray-800 dark:hover:text-gray-900 font-normal rounded-lg hover:bg-gray-100 flex items-center p-2 group "
-                >
-                  <svg
-                    className="w-6 h-6 text-gray-500 flex-shrink-0 group-hover:text-gray-900 transition duration-75"
-                    fill="currentColor"
-                    viewBox="0 0 20 20"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path d="M5 3a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2V5a2 2 0 00-2-2H5zM5 11a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2v-2a2 2 0 00-2-2H5zM11 5a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V5zM11 13a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z"></path>
-                  </svg>
-                  <span className="ml-3 flex-1 whitespace-nowrap">Kanban</span>
-                  <span className="bg-gray-200 text-gray-800 ml-3 text-sm font-medium inline-flex items-center justify-center px-2 rounded-full">
-                    Pro
+                  <span className="ml-3 flex-1 whitespace-nowrap">
+                    Dashboard
                   </span>
                 </a>
               </li>
@@ -76,10 +58,28 @@ const AsideMenu = () => {
                 <a
                   href="#"
                   target="_blank"
-                  className="text-base text-gray-900 dark:text-gray-200 hover:text-gray-800 dark:hover:text-gray-900 font-normal rounded-lg hover:bg-gray-100 flex items-center p-2 group "
+                  className="custom--link rounded-lg flex items-center p-2"
                 >
                   <svg
-                    className="w-6 h-6 text-gray-500 flex-shrink-0 group-hover:text-gray-900 transition duration-75"
+                    className="w-6 h-6 custom--svg"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path d="M5 3a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2V5a2 2 0 00-2-2H5zM5 11a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2v-2a2 2 0 00-2-2H5zM11 5a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V5zM11 13a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z"></path>
+                  </svg>
+                  <span className="ml-3 flex-1 whitespace-nowrap">Kanban</span>
+                  <span className="custom--badge">Pro</span>
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  target="_blank"
+                  className="custom--link rounded-lg flex items-center p-2"
+                >
+                  <svg
+                    className="w-6 h-6 custom--svg"
                     fill="currentColor"
                     viewBox="0 0 20 20"
                     xmlns="http://www.w3.org/2000/svg"
@@ -88,18 +88,16 @@ const AsideMenu = () => {
                     <path d="M3 5a2 2 0 012-2h1a1 1 0 010 2H5v7h2l1 2h4l1-2h2V5h-1a1 1 0 110-2h1a2 2 0 012 2v10a2 2 0 01-2 2H5a2 2 0 01-2-2V5z"></path>
                   </svg>
                   <span className="ml-3 flex-1 whitespace-nowrap">Inbox</span>
-                  <span className="bg-gray-200 text-gray-800 ml-3 text-sm font-medium inline-flex items-center justify-center px-2 rounded-full">
-                    Pro
-                  </span>
+                  <span className="custom--badge">Pro</span>
                 </a>
               </li>
               <li>
                 <a
                   href="#"
-                  className="text-base text-gray-900 dark:text-gray-200 hover:text-gray-800 dark:hover:text-gray-900 font-normal rounded-lg hover:bg-gray-100 flex items-center p-2 group "
+                  className="custom--link rounded-lg flex items-center p-2"
                 >
                   <svg
-                    className="w-6 h-6 text-gray-500 flex-shrink-0 group-hover:text-gray-900 transition duration-75"
+                    className="w-6 h-6 custom--svg"
                     fill="currentColor"
                     viewBox="0 0 20 20"
                     xmlns="http://www.w3.org/2000/svg"
@@ -116,10 +114,10 @@ const AsideMenu = () => {
               <li>
                 <a
                   href="#"
-                  className="text-base text-gray-900 dark:text-gray-200 hover:text-gray-800 dark:hover:text-gray-900 font-normal rounded-lg hover:bg-gray-100 flex items-center p-2 group "
+                  className="custom--link rounded-lg flex items-center p-2"
                 >
                   <svg
-                    className="w-6 h-6 text-gray-500 flex-shrink-0 group-hover:text-gray-900 transition duration-75"
+                    className="w-6 h-6 custom--svg"
                     fill="currentColor"
                     viewBox="0 0 20 20"
                     xmlns="http://www.w3.org/2000/svg"
@@ -138,10 +136,10 @@ const AsideMenu = () => {
               <li>
                 <a
                   href="#"
-                  className="text-base text-gray-900 dark:text-gray-200 hover:text-gray-800 dark:hover:text-gray-900 font-normal rounded-lg hover:bg-gray-100 flex items-center p-2 group "
+                  className="custom--link rounded-lg flex items-center p-2"
                 >
                   <svg
-                    className="w-6 h-6 text-gray-500 flex-shrink-0 group-hover:text-gray-900 transition duration-75"
+                    className="w-6 h-6 custom--svg"
                     fill="currentColor"
                     viewBox="0 0 20 20"
                     xmlns="http://www.w3.org/2000/svg"
@@ -158,10 +156,10 @@ const AsideMenu = () => {
               <li>
                 <a
                   href="#"
-                  className="text-base text-gray-900 dark:text-gray-200 hover:text-gray-800 dark:hover:text-gray-900 font-normal rounded-lg hover:bg-gray-100 flex items-center p-2 group "
+                  className="custom--link rounded-lg flex items-center p-2"
                 >
                   <svg
-                    className="w-6 h-6 text-gray-500 flex-shrink-0 group-hover:text-gray-900 transition duration-75"
+                    className="w-6 h-6 custom--svg"
                     fill="currentColor"
                     viewBox="0 0 20 20"
                     xmlns="http://www.w3.org/2000/svg"
@@ -179,10 +177,10 @@ const AsideMenu = () => {
             <div className="space-y-2 pt-2">
               <a
                 href="#"
-                className="text-base text-gray-900 dark:text-gray-200 hover:text-gray-800 dark:hover:text-gray-900 font-normal rounded-lg hover:bg-gray-100 group transition duration-75 flex items-center p-2"
+                className="custom--link rounded-lg flex items-center p-2"
               >
                 <svg
-                  className="w-5 h-5 text-gray-500 flex-shrink-0 group-hover:text-gray-900 transition duration-75"
+                  className="w-5 h-5 custom--svg"
                   aria-hidden="true"
                   focusable="false"
                   data-prefix="fas"
@@ -196,15 +194,17 @@ const AsideMenu = () => {
                     d="M378.7 32H133.3L256 182.7L378.7 32zM512 192l-107.4-141.3L289.6 192H512zM107.4 50.67L0 192h222.4L107.4 50.67zM244.3 474.9C247.3 478.2 251.6 480 256 480s8.653-1.828 11.67-5.062L510.6 224H1.365L244.3 474.9z"
                   ></path>
                 </svg>
-                <span className="ml-4">Upgrade to Pro</span>
+                <span className="ml-3 flex-1 whitespace-nowrap">
+                  Upgrade to Pro
+                </span>
               </a>
               <a
                 href="#"
                 target="_blank"
-                className="text-base text-gray-900 dark:text-gray-200 hover:text-gray-800 dark:hover:text-gray-900 font-normal rounded-lg hover:bg-gray-100 group transition duration-75 flex items-center p-2"
+                className="custom--link rounded-lg flex items-center p-2"
               >
                 <svg
-                  className="w-6 h-6 text-gray-500 flex-shrink-0 group-hover:text-gray-900 transition duration-75"
+                  className="w-6 h-6 custom--svg"
                   fill="currentColor"
                   viewBox="0 0 20 20"
                   xmlns="http://www.w3.org/2000/svg"
@@ -216,30 +216,34 @@ const AsideMenu = () => {
                     clipRule="evenodd"
                   ></path>
                 </svg>
-                <span className="ml-3">Documentation</span>
+                <span className="ml-3 flex-1 whitespace-nowrap">
+                  Documentation
+                </span>
               </a>
               <a
                 href="#"
                 target="_blank"
-                className="text-base text-gray-900 dark:text-gray-200 hover:text-gray-800 dark:hover:text-gray-900 font-normal rounded-lg hover:bg-gray-100 group transition duration-75 flex items-center p-2"
+                className="custom--link rounded-lg flex items-center p-2"
               >
                 <svg
-                  className="w-6 h-6 text-gray-500 flex-shrink-0 group-hover:text-gray-900 transition duration-75"
+                  className="w-6 h-6 custom--svg"
                   fill="currentColor"
                   viewBox="0 0 20 20"
                   xmlns="http://www.w3.org/2000/svg"
                 >
                   <path d="M7 3a1 1 0 000 2h6a1 1 0 100-2H7zM4 7a1 1 0 011-1h10a1 1 0 110 2H5a1 1 0 01-1-1zM2 11a2 2 0 012-2h12a2 2 0 012 2v4a2 2 0 01-2 2H4a2 2 0 01-2-2v-4z"></path>
                 </svg>
-                <span className="ml-3">Components</span>
+                <span className="ml-3 flex-1 whitespace-nowrap">
+                  Components
+                </span>
               </a>
               <a
                 href="#"
                 target="_blank"
-                className="text-base text-gray-900 dark:text-gray-200 hover:text-gray-800 dark:hover:text-gray-900 font-normal rounded-lg hover:bg-gray-100 group transition duration-75 flex items-center p-2"
+                className="custom--link rounded-lg flex items-center p-2"
               >
                 <svg
-                  className="w-6 h-6 text-gray-500 flex-shrink-0 group-hover:text-gray-900 transition duration-75"
+                  className="w-6 h-6 custom--svg"
                   fill="currentColor"
                   viewBox="0 0 20 20"
                   xmlns="http://www.w3.org/2000/svg"
@@ -250,7 +254,7 @@ const AsideMenu = () => {
                     clipRule="evenodd"
                   ></path>
                 </svg>
-                <span className="ml-3">Help</span>
+                <span className="ml-3 flex-1 whitespace-nowrap">Help</span>
               </a>
             </div>
           </div>
