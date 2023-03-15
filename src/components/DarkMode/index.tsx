f4ae3d8a30adf301179b276a1b240f9e27e665be
Walter Nascimento <walter.nascimento.barroso@gmail.com>
2023-03-15 09:58:25 +0000

    💄 UI: Adjust css

--- src/components/DarkMode/index.tsx
+++ src/components/DarkMode/index.tsx
@@ -25,7 +25,7 @@ const DarkMode = () => {
       type="button"
       aria-label="Color Mode"
       onClick={() => setDarkMode(!darkMode)}
-      className="flex justify-center p-2 text-gray-500 transition duration-150 ease-in-out bg-gray-100 border border-transparent rounded-md lg:bg-white lg:dark:bg-gray-900 dark:text-gray-200 dark:bg-gray-800 hover:bg-gray-50 dark:hover:bg-gray-700 hover:text-gray-700 focus:outline-none focus:bg-gray-50 dark:focus:bg-gray-700 active:bg-gray-50"
+      className="flex justify-center p-2 custom--button"
     >
       {darkMode ? (
         <svg
