9ef865d0f7b6a6e389d03bfe670b49fa6f1e1708
Walter Nascimento <walter.nascimento.barroso@gmail.com>
2023-03-17 15:45:01 +0000

    🎨 IMPROVE: Organize the links footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,48 +1,32 @@
 const Footer = () => {
+  const linksFooter = [
+    {
+      label: "Terms and conditions",
+      href: "",
+    },
+    {
+      label: "Privacy Policy",
+      href: "",
+    },
+    {
+      label: "Licensing",
+      href: "",
+    },
+  ];
   return (
     <>
       <footer className="bg-white dark:bg-gray-800 dark:border-gray-700 md:flex md:items-center md:justify-between shadow rounded-lg p-4 md:p-6 xl:p-8 my-6 mx-4">
         <ul className="flex items-center flex-wrap mb-6 md:mb-0">
-          <li>
+          {linksFooter.map((link: any, index: any) => (
+            <li key={index}>
             <a
-              href="#"
+              href={link.href}
               className="text-sm font-normal text-gray-500 hover:underline mr-4 md:mr-6"
             >
-              Terms and conditions
+              {link.label}
             </a>
           </li>
-          <li>
-            <a
-              href="#"
-              className="text-sm font-normal text-gray-500 hover:underline mr-4 md:mr-6"
-            >
-              Privacy Policy
-            </a>
-          </li>
-          <li>
-            <a
-              href="#"
-              className="text-sm font-normal text-gray-500 hover:underline mr-4 md:mr-6"
-            >
-              Licensing
-            </a>
-          </li>
-          <li>
-            <a
-              href="#"
-              className="text-sm font-normal text-gray-500 hover:underline mr-4 md:mr-6"
-            >
-              Cookie Policy
-            </a>
-          </li>
-          <li>
-            <a
-              href="#"
-              className="text-sm font-normal text-gray-500 hover:underline"
-            >
-              Contact
-            </a>
-          </li>
+          ))}
         </ul>
         <div className="flex sm:justify-center space-x-6">
           <a href="#" className="text-gray-500 hover:text-gray-900">
