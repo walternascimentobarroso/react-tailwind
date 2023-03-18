9ac55b9cefd5895b07df9f96266bc72fc996bffc
Walter Nascimento <walter.nascimento.barroso@gmail.com>
2023-03-18 16:00:01 +0000

    💄 UI: Login page add

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,19 +1,11 @@
-import React from "react";
-
-type ButtonProps = {
-  typeOf?: "button" | "submit" | "reset" | undefined;
-  text: string;
-  onClick: () => void;
-};
-
-const Button: React.FC<ButtonProps> = ({
-  typeOf = "button",
-  text,
-  onClick,
-}) => {
+const Button: any = ({ typeOf = "button", children, onClick }: any) => {
   return (
-    <button type={typeOf} onClick={onClick}>
-      {text}
+    <button
+      className="w-full px-4 py-2 font-bold text-white bg-blue-500 rounded-lg hover:bg-blue-700 focus:outline-none focus:shadow-outline"
+      type={typeOf}
+      onClick={onClick}
+    >
+      {children}
     </button>
   );
 };
