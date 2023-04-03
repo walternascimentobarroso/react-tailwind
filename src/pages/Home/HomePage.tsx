eb90b2834bb4ccd7d7a4f20402740724ccb72f58
Walter Nascimento <walter.nascimento.barroso@gmail.com>
2023-04-03 12:21:07 +0100

    ✨ NEW FEATURE: Update statistic and add icons

--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -1,7 +1,8 @@
+import { useState } from "react";
+import { MdGroup, MdGroupAdd, MdGroupOff, MdGroupRemove } from "react-icons/md";
+import Modal from "../../components/Modal";
 import Template from "../../components/Template";
 import Breadcrumb from "../../components/Breadcrumb";
-import Modal from "../../components/Modal";
-import { useState } from "react";
 import StatisticCard from "../../components/StatisticCard";
 
 export default () => {
@@ -27,7 +28,43 @@ export default () => {
         ]}
       />
 
-      <StatisticCard />
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 pb-4 gap-4">
+        <StatisticCard
+          background={"bg-blue-500 dark:bg-blue-800"}
+          icon={
+            <MdGroup className="w-7 h-7 text-blue-600 dark:text-blue-900" />
+          }
+          value={"4,331"}
+          description={"Total Users"}
+        />
+
+        <StatisticCard
+          background={"bg-red-500 dark:bg-red-800"}
+          icon={
+            <MdGroupRemove className="w-7 h-7 text-red-600 dark:text-red-900" />
+          }
+          value={"1,678"}
+          description={"Removed Users"}
+        />
+
+        <StatisticCard
+          background={"bg-green-500 dark:bg-green-800"}
+          icon={
+            <MdGroupAdd className="w-7 h-7 text-green-600 dark:text-green-900" />
+          }
+          value={"2,987"}
+          description={"New Users"}
+        />
+
+        <StatisticCard
+          background={"bg-orange-500 dark:bg-orange-800"}
+          icon={
+            <MdGroupOff className="w-7 h-7 text-orange-600 dark:text-orange-900" />
+          }
+          value={"1,173"}
+          description={"Inactive Users"}
+        />
+      </div>
 
       <div className="bg-white text-gray-700 dark:bg-gray-800 dark:border-gray-700 shadow rounded-lg p-4 sm:p-6 xl:p-8 ">
         <div className="mb-4 flex items-center justify-between">
