89450e9f8d41cca88169776c0570a1b53c29b7d3
Walter Nascimento <walter.nascimento.barroso@gmail.com>
2023-04-08 18:04:39 +0100

    🚧 PROGRESS: CRUD Users load data

--- src/pages/User/EditUser.tsx
+++ src/pages/User/EditUser.tsx
@@ -1,42 +1,70 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Input from "../../components/Input";
 import Button from "../../components/Button";
 import { useModal } from "../../hooks/useModal";
 import ProfilePicture from "../../components/ProfilePicture";
+import Alert from "../../components/Alert";
 
 export default ({ id, editRow, defaultValue }: any) => {
   const { openModal, closeModal, ModalWrapper } = useModal();
 
-  const [formState, setFormState] = useState(
-    defaultValue || {
-      name: "",
-      lastName: "",
-      email: "",
-      password: "",
-    }
-  );
+  const [formState, setFormState] = useState({
+    name: "",
+    lastName: "",
+    email: "",
+    password: "",
+  });
 
-  const handleChange = (e: any) =>
+  const [errors, setErrors] = useState("");
+
+  useEffect(() => setFormState(defaultValue), [defaultValue]);
+
+  const handleChange = (e: any) => {
+    if (!formState) return;
     setFormState({ ...formState, [e.target.name]: e.target.value });
+  };
+
+  const validateForm = () => {
+    setErrors("");
+    let errorFields: any = [];
+    for (const [key, value] of Object.entries(formState)) {
+      if (!value) {
+        errorFields.push(key);
+      }
+    }
+    if (errorFields.length) {
+      setErrors(errorFields.join(", "));
+      return false;
+    }
+    return true;
+  };
 
   const handleSubmit = (e: any) => {
-    // if (!validateForm()) return;
+    if (!validateForm()) return;
 
-    console.log(formState);
+    console.log("defaultValue", defaultValue);
+    console.log("form", formState);
   };
 
   return (
     <>
-      <button
-        className="custom--btn-warning mr-4"
+      <Button
+        customClass="custom--btn-warning mr-4"
         onClick={() => {
           editRow(id);
           openModal();
         }}
       >
         Edit
-      </button>
+      </Button>
       <ModalWrapper title="Edit User">
+        {errors && (
+          <Alert
+            type={"Error"}
+            message={`Please include: ${errors}`}
+            onClick={() => setErrors("")}
+          />
+        )}
         <form className="p-1 bg-white dark:bg-gray-800">
           <div className="flex justify-center">
             <ProfilePicture alt="Profile picture" />
@@ -46,7 +74,7 @@ export default ({ id, editRow, defaultValue }: any) => {
             label={"Name"}
             placeholder={"Name"}
             name={"name"}
-            value={formState.name}
+            value={formState.name || ""}
             onChange={handleChange}
           />
 
@@ -54,7 +82,7 @@ export default ({ id, editRow, defaultValue }: any) => {
             label={"Last Name"}
             placeholder={"Last Name"}
             name={"lastName"}
-            value={formState.lastName}
+            value={formState.lastName || ""}
             onChange={handleChange}
           />
 
@@ -63,7 +91,7 @@ export default ({ id, editRow, defaultValue }: any) => {
             type={"email"}
             placeholder={"Email"}
             name={"email"}
-            value={formState.email}
+            value={formState.email || ""}
             onChange={handleChange}
           />
 
@@ -72,7 +100,7 @@ export default ({ id, editRow, defaultValue }: any) => {
             type={"password"}
             placeholder={"Password"}
             name={"password"}
-            value={formState.password}
+            value={formState.password || ""}
             onChange={handleChange}
           />
 
