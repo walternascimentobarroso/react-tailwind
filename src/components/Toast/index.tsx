bf433a71b070fffab889e7bfdbe60a3c2223c005
Walter Nascimento <walter.nascimento.barroso@gmail.com>
2023-04-15 22:47:10 +0100

    🔨 REFACTOR: Color for toast

--- src/components/Toast/index.tsx
+++ src/components/Toast/index.tsx
@@ -3,9 +3,10 @@ import React, { useState, useEffect } from "react";
 interface ToastProps {
   message: string;
   duration: number;
+  type?: "danger" | "warning" | "success" | "info";
 }
 
-const Toast: React.FC<ToastProps> = ({ message, duration }) => {
+const Toast: React.FC<ToastProps> = ({ message, duration, type = "info" }) => {
   const [showToast, setShowToast] = useState(false);
 
   useEffect(() => {
@@ -20,11 +21,28 @@ const Toast: React.FC<ToastProps> = ({ message, duration }) => {
     };
   }, []);
 
+  let bgColor = "";
+  switch (type) {
+    case "danger":
+      bgColor = "bg-red-500";
+      break;
+    case "warning":
+      bgColor = "bg-yellow-500";
+      break;
+    case "success":
+      bgColor = "bg-green-500";
+      break;
+    case "info":
+    default:
+      bgColor = "bg-blue-500";
+      break;
+  }
+
   return (
     <div
-      className={`fixed bottom-0 right-0 mb-4 mr-4 p-4 rounded-md bg-gray-800 text-white ${
+      className={`fixed top-0 right-0 mt-20 mr-4 p-4 rounded-md text-white ${bgColor} ${
         showToast ? "opacity-100" : "opacity-0 pointer-events-none"
-      } transition-opacity duration-300`}
+      } transition-opacity duration-300 z-30`}
     >
       {message}
     </div>
